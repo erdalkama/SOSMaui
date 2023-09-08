2d220b904f16899154557e4201dcadabb94d8618
erdalkama <erdalkama@hotmail.com>
2023-09-08 09:38:29 +0300

    SOS Function

--- panicButton/MainPage.xaml.cs
+++ panicButton/MainPage.xaml.cs
@@ -1,25 +1,38 @@
-﻿namespace panicButton;
+﻿using System.Globalization;
+namespace panicButton;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public MainPage()
+    {
+        InitializeComponent();
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    }
+    async void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+        var status = await Permissions.CheckStatusAsync<Permissions.Sms>();
+        string number = TelephoneNumber.Text;
+        var location = await Geolocation.Default.GetLocationAsync();
+        var point = new Point(location.Latitude, location.Longitude);
+        string[] recipients = new[] { number };
+        if (!string.IsNullOrWhiteSpace(number))
+        {
+            if (Sms.Default.IsComposeSupported)
+            {
+                var numberFormatInfo = new NumberFormatInfo { NumberDecimalSeparator = "." };
+                string text = $"I need help here my location\n https://www.google.com/maps/search/?api=1&query={location.Latitude.ToString(numberFormatInfo)},{location.Longitude.ToString(numberFormatInfo)}";
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+                var messsage = new SmsMessage(text, recipients);
+                await Sms.Default.ComposeAsync(messsage);
+                 
+                await DisplayAlert("SUCCESS", $"Your location sent to {number}!", "OK");
+            }
+        }
+        else
+        {
+            await DisplayAlert("ERROR", "You need to enter the number!", "OK");
+        }
+    }
 }
 
 
